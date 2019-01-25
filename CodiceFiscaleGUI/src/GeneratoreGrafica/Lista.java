c1db8ba38bd298ae4cd9471b0d933b6d50a0d6f4
Stefano Valloncini <stevattthebest@gmail.com>
2019-01-25 23:03:16 +0100

    Update Lista.java

--- CodiceFiscaleGUI/src/GeneratoreGrafica/Lista.java
+++ CodiceFiscaleGUI/src/GeneratoreGrafica/Lista.java
@@ -1,9 +1,14 @@
 package GeneratoreGrafica;
 import java.awt.*; 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import javax.swing.*; 
 import java.util.Scanner; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author xStevatt
@@ -28,12 +33,30 @@ public class Lista extends JFrame
         JPanel descrizione = new JPanel(); 
         descrizione.setBackground(new Color(238, 130, 238));
         
-        JPanel lista = new JPanel(); 
+        JPanel lista = new JPanel(new BorderLayout()); 
         lista.setBackground(new Color(238, 130, 238));
         
         JLabel titolo = new JLabel("Lista dei codici fiscali: "); 
         titolo.setFont(new Font("Arial", Font.ITALIC + Font.BOLD, 30));
         descrizione.add(titolo); 
+        
+        JButton bottonePulizia = new JButton("Canc"); 
+        
+        bottonePulizia.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+              PrintWriter writer = null; 
+                try {
+                    writer = new PrintWriter(new File("codicifiscali.txt"));
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(Lista.class.getName()).log(Level.SEVERE, null, ex);
+                }
+              writer.write("I codici verrano salvati qui: \n");
+            }
+        });
+        
+        lista.add(bottonePulizia, BorderLayout.SOUTH);
         
         text = new JTextArea(); 
         text.setEditable(false);
